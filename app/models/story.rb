4775393f57714b06cd8eae9451dc927cb0766ffe
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-01-14 16:11:37 +0400

    fixed code style

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,13 +2,15 @@ class Story < ActiveRecord::Base
   has_many :story_comments
   belongs_to :user
 
-  STATUSES = { 0 => 'new',
-               1 => 'started',
-               2 => 'finished',
-               3 => 'accepted',
-               4 => 'rejected' }
+  STATUS = {
+    0 => 'new',
+    1 => 'started',
+    2 => 'finished',
+    3 => 'accepted',
+    4 => 'rejected'
+  }
 
   def getStatus
-    STATUSES[@status]
+    STATUS[@status]
   end
 end
