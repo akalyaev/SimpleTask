a5ef4f09470bebc57443e18625bec4c196ef2457
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-01-14 18:12:03 +0400

    changed story layout

--- spec/views/stories/index.html.haml_spec.rb
+++ spec/views/stories/index.html.haml_spec.rb
@@ -25,8 +25,8 @@ describe "stories/index.html.haml" do
   it "renders a list of stories" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "h3", :text => "Name".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "pre", :text => "MyText".to_s, :count => 2
   end
 end
