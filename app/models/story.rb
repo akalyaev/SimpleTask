f6492375ff253891ac4bd880e5ee498627c107db
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-01-14 16:08:06 +0400

    added statuses array

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,14 @@
 class Story < ActiveRecord::Base
   has_many :story_comments
   belongs_to :user
+
+  STATUSES = { 0 => 'new',
+               1 => 'started',
+               2 => 'finished',
+               3 => 'accepted',
+               4 => 'rejected' }
+
+  def getStatus
+    STATUSES[@status]
+  end
 end
