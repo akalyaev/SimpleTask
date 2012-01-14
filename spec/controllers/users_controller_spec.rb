b9a1b9b85f673297be062e614e398f8f867cf6d6
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-01-14 16:16:19 +0400

    fixed valid_attributes for the controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,7 +24,7 @@ describe UsersController do
   # User. As you add validations to User, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    { :username => 'test', :salt => '', :password => 'test' }
   end
 
   describe "GET index" do
