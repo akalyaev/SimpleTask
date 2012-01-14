df57f6c75fe621b78480665bb41ca6d242c7f531
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-01-14 17:59:45 +0400

    cleaned up tests

--- spec/views/stories/index.html.haml_spec.rb
+++ spec/views/stories/index.html.haml_spec.rb
@@ -28,13 +28,5 @@ describe "stories/index.html.haml" do
     assert_select "tr>td", :text => "Name".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 2.to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 3.to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 4.to_s, :count => 2
   end
 end
--- spec/views/users/index.html.haml_spec.rb
+++ spec/views/users/index.html.haml_spec.rb
@@ -28,7 +28,5 @@ describe "users/index.html.haml" do
     assert_select "tr>td", :text => "Salt".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Password".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => false.to_s, :count => 4
   end
 end
