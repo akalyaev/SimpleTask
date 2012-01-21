4c3f62c8146e292393e70cd6bf2552bde5ed7f5d
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-01-21 12:53:02 +0400

    updated test

--- spec/views/stories/index.html.haml_spec.rb
+++ spec/views/stories/index.html.haml_spec.rb
@@ -27,6 +27,6 @@ describe "stories/index.html.haml" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "h3", :text => "Name".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "pre", :text => "MyText".to_s, :count => 2
+    assert_select ".description", :text => "MyText".to_s, :count => 2
   end
 end
