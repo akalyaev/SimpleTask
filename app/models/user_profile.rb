c1cada80df8ab97ec5ccad6fab8d5d55c5b7fbe3
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-01-21 14:21:04 +0400

    added full_name method

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,3 +1,9 @@
 class UserProfile < ActiveRecord::Base
   belongs_to :user
+
+  def full_name(reverse=false)
+    reverse ?
+      "#{self.given_names} #{self.surname}" :
+      "#{self.surname}, #{self.given_names}"
+  end
 end
