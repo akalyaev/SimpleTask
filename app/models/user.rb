a9cf5edbdfba5c27e6edbe6923a907b5026e516e
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-01-21 15:38:59 +0400

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   has_one :developer
   has_many :stories
   has_many :story_comments
+
+  validates_presence_of :username
+  validates_presence_of :password
+  validates_inclusion_of :is_active, :in => [true, false]
+  validates_inclusion_of :is_super_admin, :in => [true, false]
 end
