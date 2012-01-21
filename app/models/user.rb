940cc72a71f53c4af49a753ac48af1f0194efe17
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-01-21 15:44:15 +0400

    updated validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ActiveRecord::Base
   has_many :story_comments
 
   validates_presence_of :username
+  validates_length_of :username, :maximum => 128
+
   validates_presence_of :password
+  validates_length_of :password, :maximum => 128
+
   validates_inclusion_of :is_active, :in => [true, false]
   validates_inclusion_of :is_super_admin, :in => [true, false]
 end
