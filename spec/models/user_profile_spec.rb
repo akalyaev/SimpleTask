1a6c448e2d624c6b62861552de4fc6c7f59e8c93
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-01-28 17:18:45 +0400

    added tests for user_profile model

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe UserProfile do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = User.create({ :username => 'test', :salt => '', :password => 'test' })
+    @valid_attributes = { user_id: user.id, surname: "Doe", given_names: "John",
+                          gender: 0, phone: "444-444", mail: "admin@st.com" }
+  end
+
+  it "returns proper fullname" do
+    profile = UserProfile.create! @valid_attributes
+    profile.full_name(false).should == "John Doe"
+    profile.full_name(true).should == "Doe, John"
+  end
 end
