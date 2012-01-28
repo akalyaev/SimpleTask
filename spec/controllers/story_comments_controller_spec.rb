fe0dab4847d5f66ead02d5f75bbbd65e2b12ecae
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-01-28 16:57:29 +0400

    fixed comments controller tests

--- spec/controllers/story_comments_controller_spec.rb
+++ spec/controllers/story_comments_controller_spec.rb
@@ -5,27 +5,27 @@ describe StoryCommentsController do
   before do
     @story = Story.create({ :name => 'test' })
     @user = User.create({ :username => 'test', :salt => '', :password => 'test' })
-    @valid_attributes = { :story_id => @story.id, :user_id => @user.id }
+    @valid_attributes = { :comment => 'Test comment' }
   end
 
   describe "GET 'create'" do
     describe "with valid params" do
-      #it "creates a new comment" do
-      #  expect {
-      #    post :create, :story_comment => @valid_attributes
-      #  }.to change(StoryComment, :count).by(1)
-      #end
+      it "creates a new comment" do
+        expect {
+          post :create, :story_id => @story.id, :story_comment => @valid_attributes
+        }.to change(StoryComment, :count).by(1)
+      end
 
-      #it "assigns a newly created comment as @story_comment" do
-      #  post :create, :story_id => @story.id, :story_comment => @valid_attributes
-      #  assigns(:story_comment).should be_a(StoryComment)
-      #  assigns(:story_comment).should be_persisted
-      #end
-      #
-      #it "redirects to the story" do
-      #  post :create, :story_id => @story.id, :story_comment => @valid_attributes
-      #  response.should redirect_to(@story)
-      #end
+      it "assigns a newly created comment as @story_comment" do
+        post :create, :story_id => @story.id, :story_comment => @valid_attributes
+        assigns(:story_comment).should be_a(StoryComment)
+        assigns(:story_comment).should be_persisted
+      end
+
+      it "redirects to the story" do
+        post :create, :story_id => @story.id, :story_comment => @valid_attributes
+        response.should redirect_to(@story)
+      end
     end
   end
 
