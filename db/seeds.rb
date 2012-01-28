5df3ad10877a265cd80d2df0836daa13fc0cf6c3
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-01-28 11:56:59 +0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 users = User.create([{ username: 'admin', salt: '', password: 'admin', is_super_admin: true },
-                     { username: 'developer', salt: '', password: 'developer' }])
+                     { username: 'developer1', salt: '', password: 'developer' },
+                     { username: 'developer2', salt: '', password: 'developer' }])
 
-profiles = UserProfile.create([{ user: users.first, surname: "Doe", given_names: "John",
+profiles = UserProfile.create([{ user: users[0], surname: "Doe", given_names: "John",
                                  gender: 0, phone: "444-444", mail: "admin@st.com" },
-                               { user: users.last, surname: "Doe", given_names: "Nick",
-                                 gender: 0, phone: "444-444", mail: "nick@st.com" }])
+                               { user: users[1], surname: "Doe", given_names: "Nick",
+                                 gender: 0, phone: "444-444", mail: "nick@st.com" },
+                               { user: users[2], surname: "Lucas", given_names: "Smith",
+                                 gender: 0, phone: "444-444", mail: "lucas@st.com" }])
 
-developers = Developer.create({ user: users.last, busy: 0 })
+developers = Developer.create([{ user: users[1], busy: 1 }, { user: users[2], busy: 0 }])
 
-stories = Story.create({ name: 'Task 1', description: 'Test description.' })
+stories = Story.create([{ name: 'Task 1', description: 'Test description.', user: user[1], points: 5 },
+                        { name: 'Task 2', description: 'Test description.', points: 5 },
+                        { name: 'Task in backlog', description: 'Test description.', points: 2, active: false }])
