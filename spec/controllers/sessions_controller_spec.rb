543a52eb8f4c683f821b2784c740c9d4f6f98786
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-02-18 15:34:52 +0400

    added tests for session controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe SessionsController do
+  before do
+    @user = User.create({ :username => 'test', :password => 'test' })
+  end
 
   describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
+    describe "with valid params" do
+      it "redirects to the root path" do
+        post :create, :username => 'test', :password => 'test'
+        response.should redirect_to(root_path)
+      end
+    end
+
+    describe "with invalid params" do
+      it "re-renders the 'new' template" do
+        post :create, :username => 'test', :password => 'invalid_password'
+        response.should render_template("new")
+      end
     end
   end
 
