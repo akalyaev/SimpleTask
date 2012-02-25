e4cb9ab2a02b6d48ad11df99b2a605a66edcf1bd
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-02-25 12:08:13 +0400

    rewrited developer spec using factory_girl

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -1,14 +1,10 @@
 require 'spec_helper'
+require 'factory_girl'
 
 describe UserProfile do
-  before do
-    user = User.create({ :username => 'test', :salt => '', :password => 'test' })
-    @valid_attributes = { user_id: user.id, surname: "Doe", given_names: "John",
-                          gender: 0, phone: "444-444", mail: "admin@st.com" }
-  end
 
   it "returns proper fullname" do
-    profile = UserProfile.create! @valid_attributes
+    profile = FactoryGirl.build(:user_profile, :surname => 'Doe', :given_names => 'John')
     profile.full_name(false).should == "John Doe"
     profile.full_name(true).should == "Doe, John"
   end
