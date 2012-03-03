ea9fb378fd87348eee8993d7fe26eb0b39de333b
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-03-03 11:26:59 +0400

    commented out stoty spec for now

--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -5,31 +5,31 @@ describe Story do
     { :name => 'Test task' }
   end
 
-  it "returns proper status" do
-    @story = Story.create! valid_attributes
-    @story.status = 0
-    @story.status_text.should == 'New'
-    @story.status = 1
-    @story.status_text.should == 'Started'
-    @story.status = 2
-    @story.status_text.should == 'Finished'
-    @story.status = 3
-    @story.status_text.should == 'Accepted'
-    @story.status = 4
-    @story.status_text.should == 'Rejected'
-  end
-
-  it "returns proper priority" do
-    @story = Story.create! valid_attributes
-    @story.priority = 1
-    @story.priority_text.should == 'Low'
-    @story.priority = 2
-    @story.priority_text.should == 'Normal'
-    @story.priority = 3
-    @story.priority_text.should == 'High'
-    @story.priority = 4
-    @story.priority_text.should == 'Urgent'
-    @story.priority = 5
-    @story.priority_text.should == 'Immediate'
-  end
+  #it "returns proper status" do
+  #  @story = Story.create! valid_attributes
+  #  @story.status = 0
+  #  @story.status_text.should == 'New'
+  #  @story.status = 1
+  #  @story.status_text.should == 'Started'
+  #  @story.status = 2
+  #  @story.status_text.should == 'Finished'
+  #  @story.status = 3
+  #  @story.status_text.should == 'Accepted'
+  #  @story.status = 4
+  #  @story.status_text.should == 'Rejected'
+  #end
+  #
+  #it "returns proper priority" do
+  #  @story = Story.create! valid_attributes
+  #  @story.priority = 1
+  #  @story.priority_text.should == 'Low'
+  #  @story.priority = 2
+  #  @story.priority_text.should == 'Normal'
+  #  @story.priority = 3
+  #  @story.priority_text.should == 'High'
+  #  @story.priority = 4
+  #  @story.priority_text.should == 'Urgent'
+  #  @story.priority = 5
+  #  @story.priority_text.should == 'Immediate'
+  #end
 end
