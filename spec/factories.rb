4a91fe4ab6758fc007c351ab34591b08b3ed0e99
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-03-03 15:29:13 +0400

    added tests for story state_machine

--- spec/factories.rb
+++ spec/factories.rb
@@ -21,40 +21,7 @@ FactoryGirl.define do
   end
 
   factory :story do
-    name 'test'
-
-    trait :assigned do
-      assigned_to
-    end
-
-    trait :in_backlog do
-      active false
-    end
-
-    trait :new do
-      status 'new'
-    end
-
-    trait :started do
-      status 'started'
-    end
-
-    trait :finished do
-      status 'finished'
-    end
-
-    trait :accepted do
-      status 'accepted'
-    end
-
-    trait :rejected do
-      status 'rejected'
-    end
-
-    factory :new_story, :traits => [:new]
-    factory :new_story_in_backlog, :traits => [:new, :in_backlog]
-    factory :accepted_story, :traits => [:accepted, :assigned]
-    factory :rejected_story, :traits => [:rejected]
+    name 'Test story'
   end
 
   factory :story_comment do
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -3,37 +3,55 @@ require 'factory_girl'
 
 describe Story do
 
-  describe "states" do
-    before(:all) do
+  describe "states:" do
+    before(:each) do
+      @story = FactoryGirl.create(:story)
       @user = FactoryGirl.create(:user)
     end
 
-    after(:all) do
+    after(:each) do
+      @story.destroy
       @user.destroy
     end
 
     describe "new" do
-      before(:all) do
-        @story = FactoryGirl.create(:new_story)
-      end
-
-      after(:all) do
-        @story.destroy
-      end
-
       it "should be an initial state" do
         @story.new?.should be_true
       end
 
       it "should change to :accepted on :accept if assigned" do
         @story.user = @user
-        @story.accept!
+        @story.accept
         @story.accepted?.should be_true
       end
 
       it "should raise error if not assigned" do
         @story.user = nil
         lambda {@story.accept!}.should raise_error
+      end
+    end
+
+    describe "accepted" do
+      before(:each) do
+        @story.user = @user
+        @story.accept
+      end
+
+      it "should change to rejected on :reject" do
+        @story.reject
+        @story.rejected?.should be_true
+      end
+    end
+
+    describe "rejected" do
+      before(:each) do
+        @story.user = @user
+        @story.accept
+        @story.reject
+      end
+
+      it "should have nil user" do
+        @story.user.should be_nil
       end
     end
   end
