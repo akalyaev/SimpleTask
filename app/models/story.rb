8b7cabfb6213200b1b5b6258561d99a9d4b6a434
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-03-03 11:27:23 +0400

    refactored status field

--- app/models/story.rb
+++ app/models/story.rb
@@ -12,13 +12,7 @@ class Story < ActiveRecord::Base
   POINT_MIN = 1
   POINT_MAX = 5
 
-  STATUSES = {
-    0 => 'New',
-    1 => 'Started',
-    2 => 'Finished',
-    3 => 'Accepted',
-    4 => 'Rejected'
-  }
+  STATUSES = [:new, :started, :finished, :accepted, :rejected]
 
   PRIORITIES = {
     1 => 'Low',
@@ -30,14 +24,8 @@ class Story < ActiveRecord::Base
 
   # validation
   validates :name, :presence => true, :length => {:maximum => 128}
-  validates :status,   :numericality => true, :length => {:within => 0..4}
   validates :points,   :numericality => true, :length => {:within => POINT_MIN..POINT_MAX}
   validates :priority, :numericality => true, :length => {:within => 1..5}
-
-  # get options section
-  def self.status_options
-    STATUSES.invert
-  end
 
   def self.user_options
     developers = Developer.includes(:user_profile)
@@ -51,10 +39,6 @@ class Story < ActiveRecord::Base
 
   def self.priority_options
     PRIORITIES.invert
-  end
-
-  def status_text
-    STATUSES[self.status]
   end
 
   def priority_text
@@ -80,7 +64,7 @@ class Story < ActiveRecord::Base
     total = 0
     grouped_data = Story.where(:active => true).group(:status).count
     grouped_data.each do |status, count|
-      data[STATUSES[status]] = count
+      data[status] = count
       total += count
     end
     data['All'] = total
