f0c1e6a37d43a439ed4ad26928ce4a00c8cdcc4e
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-03-04 15:59:33 +0400

    added helper for move action

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,10 @@
 module StoriesHelper
+
+  def link_to_move(story, html_options)
+    if story.can_move_to_backlog?
+      link_to 'Move to backlog', move_story_path(story), html_options
+    elsif story.can_move_to_active_sprint?
+      link_to 'Move to active sprint', move_story_path(story), html_options
+    end
+  end
 end
