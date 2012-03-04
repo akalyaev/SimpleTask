ad508b83534d3157e2cc6419dc92e6e969f9c448
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-03-04 16:14:20 +0400

    fixed helper declaration

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,6 +1,6 @@
 module StoriesHelper
 
-  def link_to_move(story, html_options)
+  def link_to_move(story, html_options = {})
     if story.can_move_to_backlog?
       link_to 'Move to backlog', move_story_path(story), html_options
     elsif story.can_move_to_active_sprint?
