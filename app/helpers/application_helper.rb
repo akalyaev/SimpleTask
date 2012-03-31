9ca5d37ab9c85cef45d2421fec1c0c66dbdd5010
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-03-31 18:34:59 +0400

    add highligthing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def item(tag, name, path, link_options = {})
+    options = {}
+    options[:class] = :active if current_page?(path)
+    link = link_to(name, path, link_options)
+    content_tag(:li, link, options)
+  end
 end
